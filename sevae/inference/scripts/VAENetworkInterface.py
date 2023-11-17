6aaeac585412dc7a2ccfc697342a208cec5af726
Mathias Skogli <mskogli@student.ethz.ch>
2023-11-17 16:18:38 +0100

    Add batch dim

--- sevae/inference/scripts/VAENetworkInterface.py
+++ sevae/inference/scripts/VAENetworkInterface.py
@@ -13,7 +13,7 @@ COLL_REP_PATH = sevae.__path__[0]
 WEIGHTS_PATH = COLL_REP_PATH + "/weights/seVAE_weights/seVAE_evo_LD_128.pth"
 
 
-class VAENetworkInterface():
+class VAENetworkInterface:
     def __init__(self, latent_space_dims=128, device="cuda:0"):
         self.latent_space = latent_space_dims
         self.device = device
@@ -21,7 +21,7 @@ class VAENetworkInterface():
         # Load networks in constructor
         try:
             self.vae = VAE(input_dim=1, latent_dim=self.latent_space)
-            dict = torch.load(WEIGHTS_PATH)
+            dict = torch.load(WEIGHTS_PATH, map_location=torch.device("cpu"))
             self.vae.load_state_dict(dict, strict=True)
             self.vae = self.vae.to(self.device)
         except:
@@ -35,12 +35,21 @@ class VAENetworkInterface():
         with torch.no_grad():
             torch_image = torch.from_numpy(image_numpy).float().to(self.device)
             torch_image = torch.clamp(torch_image, 0.0, 1.0)
-            z_sampled, means, *_ = self.vae.encode(torch_image.view(1, 1, 270, 480))
+            z_sampled, means, *_ = self.vae.encode(torch_image.view(-1, 1, 270, 480))
             reconstructed_image = 0
             if calculate_reconstruction:
                 reconstructed_image = self.vae.decode(means).cpu().numpy()
         self.end_time = time.time()
-        return means.cpu().numpy(), reconstructed_image, 1000*(self.end_time - self.start_time)
-    
+        return (
+            means.cpu().numpy(),
+            reconstructed_image,
+            1000 * (self.end_time - self.start_time),
+        )
+
+    def decode(self, latent):
+        with torch.no_grad():
+            reconstructed_image = self.vae.decode(latent).cpu().numpy()
+        return reconstructed_image
+
     def get_compute_time(self):
-        return (self.end_time - self.start_time)*1000
+        return (self.end_time - self.start_time) * 1000
